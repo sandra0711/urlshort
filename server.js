a5b21d46e640a92ffac72fa7bde36f26d721d035
rom <filpoyma@gmail.com>
2020-08-23 17:50:19 +0300

    npm update

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app');
+const app = require("./app");
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Started on ${PORT}`));
+//********************* MONGOOSE CONNECTION
+
+// const dbName = 'urlShorter';
+// const dbPath = `mongodb://localhost/${dbName}`;
+//
+// mongoose.connect(dbPath, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(()=> {console.log('Mongoose connected to %s name database', dbName)})
+//   .catch((err)=> {console.log('Database connection error', err.message)});
+
+//********************** END
+
+app.listen(port, () => {
+  console.log("Server started at http://localhost:%s/", port);
+});
