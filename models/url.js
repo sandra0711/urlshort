832e0fdc6ecf6b92d1cba1a7e99b50c431861cba
rom <filpoyma@gmail.com>
2020-08-23 18:08:04 +0300

    npm update

---
+++ models/url.js
@@ -0,0 +1,9 @@
+// const mongoose = require("mongoose");
+// const { Schema, model } = mongoose;
+//
+// const urlSchema = new Schema({
+//
+// });
+//
+//
+// module.exports = model("Url", urlSchema);
